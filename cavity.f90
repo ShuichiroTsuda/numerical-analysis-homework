2c4a341042139c1d3e0ed45602dc49f24f54b7f2
ShuichiroTsuda <shuichiro715@outlook.jp>
2021-05-23 22:26:21 +0900

    Add vector grach

--- cavity.f90
+++ cavity.f90
@@ -6,16 +6,21 @@ program cavity
         double precision u, v
     end type latticePoint
 
-    integer, parameter :: latticeSizeX = 10
-    integer, parameter :: latticeSizeY = 10
+    integer, parameter :: latticeSizeX = 50
+    integer, parameter :: latticeSizeY = 50
 
     double precision, parameter :: reynolds = 10
 
     double precision :: h = 1.0 / latticeSizeX
     
     type(latticePoint) :: latticePoints(latticeSizeX, latticeSizeY)
+    type(latticePoint) :: lastLaticePoint !収束判断のための直前の値
 
     integer :: i, j, k, l, m
+
+    logical :: shouldContinue
+
+    double precision, parameter :: convergenceThreshold = 0.00001
 
     latticePoints = latticePoint(phi=0.0, omega=0.0, u=0.0, v=0.0)
 
@@ -24,10 +29,12 @@ program cavity
         write(11, '(*(f0.0:" "))') latticePoints(:, i)%u
     end do
     close(11)
-
-    do m = 1, 100
+    
+    do m = 1, 5000
+        shouldContinue = .false.
         do i = 1, latticeSizeX
             do j = 1, latticeSizeY
+                lastLaticePoint = latticePoints(i, j)
                 if (i == 1) then 
                     latticePoints(i, j)%phi = 0
                     latticePoints(i, j)%omega = 2.0 * latticePoints(i + 1, j)%phi
@@ -59,6 +66,7 @@ program cavity
                         latticePoints(i, j-1)%phi - &
                         latticePoints(i, j)%omega) / 4.0
                 end if
+                !shouldContinue = shouldContinue.or.lastLaticePoint(i, j)%phi
             end do
         end do
     end do
@@ -69,18 +77,72 @@ program cavity
             latticePoints(i, j)%omega = - latticePoints(i, j)%omega / h ** 2.0
         end do
     end do
+
+
+    do i = 1, latticeSizeX
+        do j = 1, latticeSizeY
+            if (i == 1 .or. i == latticeSizeX .or. j == 1) then 
+                latticePoints(i, j)%u = 0.0
+                latticePoints(i, j)%v = 0.0
+
+            else if (j == latticeSizeY) then 
+                latticePoints(i, j)%u = -1.0
+                latticePoints(i, j)%v = 0.0
+            else
+                latticePoints(i, j)%u = &
+                    (latticePoints(i, j+1)%phi - latticePoints(i, j-1)%phi) / &
+                    h / 2
+                latticePoints(i, j)%v = &
+                    - (latticePoints(i+1, j)%phi - latticePoints(i-1, j)%phi) / &
+                    h / 2
+            end if
+        end do
+    end do
     
-    
-    open(10, file='cavity_u.csv')
+    open(10, file='cavity_phi.csv')
     do k = 1, latticeSizeY
-        write(10, '(*(f0.10:" "))') latticePoints(:, k)%u
+        write(10, '(*(f0.10:" "))') latticePoints(:, k)%phi
     end do
     close(10)
 
-    open(12, file='cavity_v.csv')
+    open(11, file='cavity_omega.csv')
     do l = 1, latticeSizeY
-        write(12, '(*(f0.10:" "))') latticePoints(:, l)%v
+        write(12, '(*(f0.10:" "))') latticePoints(:, l)%omega
+    end do
+    close(11)
+
+    open(12, file='cavity_u.csv')
+    do i = 1, latticeSizeX
+        do j = 1, latticeSizeY
+            write (12,'(3e12.4)') i*h, j*h, latticePoints(i, j)%u
+        end do
     end do
     close(12)
+
+    open(13, file='cavity_v.csv')
+    do i = 1, latticeSizeX
+        do j = 1, latticeSizeY
+            write (13,'(3e12.4)') i*h, j*h, latticePoints(i, j)%v
+        end do
+    end do
+    close(13)
+
+    open(14, file='cavity_velocity.csv')
+    do i = 1, latticeSizeX
+        do j = 1, latticeSizeY
+            write (14,'(4e12.4)') i*h, j*h, latticePoints(i, j)%u, latticePoints(i, j)%v
+        end do
+    end do
+    close(14)
+
+    stop
+    contains
+    
+    logical function isUnconverged(a, b)
+        implicit none
+        double precision :: a, b 
+        isUnconverged = abs(a-b) > convergenceThreshold
+        return
+    end function isUnconverged
 
 end program cavity
